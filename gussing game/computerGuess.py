33e6b78ac1676720a9eaaf2ba02a80f5d8f91e79
Praveen Thamindu <praveenthamindu@gmail.com>
2022-03-23 19:32:24 +0530

    finish computer guessing game

--- gussing game/computerGuess.py
+++ gussing game/computerGuess.py
@@ -1,8 +1,15 @@
-max_number = int(input("What is the range > 1 to : "))
+print("Welcome to the Guessing Game, my friend. First, how does this work? First, you give the range and you think of one number in that range. (0 - your number)\n")
+
+print("How to play")
+print("if number is low you enter > 'l'")
+print("if number is high you enter > 'h'")
+print("if i guess the correct you enter > 'c'")
+
+max_number = int(input("What is the range > 0 to : "))
 
 
 def computer_guess(min, max):
-    middle_number = int((max-min) / 2)
+    middle_number = int(round((min + max) / 2, 0))
     return middle_number
 
 
@@ -15,25 +22,28 @@ def display_answer(answer):
 def guess_my_number(x):
     won = False
     max = x
-    low = 1
-    inputs = {
-        "l" : "Low",
-        "h" : "high",
-        "c" : "Currect"
-    }
+    min = 0
+
+    computers_guess = computer_guess(min, max)
+    user_answer = display_answer(computers_guess)
 
     while not won: 
-        computers_guess = computer_guess(low, max)
-        user_answer = display_answer(computers_guess)
         if user_answer == "c":
             won = True
             print("Yeeee!!! I won")
             return
         elif user_answer == "h":
+            print('"OOPs!"\n')
             min = computers_guess
+            computers_guess = computer_guess(min, max)
+            user_answer = display_answer(computers_guess)
+        elif user_answer == "l":
+            print('"OPPs!"\n')
+            max = computers_guess
+            computers_guess = computer_guess(min, max)
+            user_answer = display_answer(computers_guess)
+        else:
+            print("I don't undestand! did you read the instructions??")
 
         
-
-
-
 guess_my_number(max_number)
