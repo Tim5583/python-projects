dca71543e08635cea7d6129029d7b2d94f7b760d
Praveen Thamindu <praveenthamindu@gmail.com>
2022-03-28 19:44:42 +0530

    set user can only enter one letter

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -9,7 +9,6 @@ def random_word():
 
 
 def handman():
-    # get random word
     print("Welcome to Hangman")
     print("""
 
@@ -33,7 +32,13 @@ def handman():
         print(f"You have {lives} lives left\n")
         print("Word >> "+"".join([f" {letter} " if letter in users_letters else " # " for letter in chosen_word]))
         user_guess_letter = (input("Guess a letter: ")).upper()
-        users_letters.append(user_guess_letter)
+
+        if len(user_guess_letter) > 1:
+            print("You must only enter one letter at a time")
+            user_guess_letter = user_guess_letter[0]
+
+        if user_guess_letter == "":
+            continue
 
         if user_guess_letter in chosen_word_letters:
             chosen_word_letters.discard(user_guess_letter)
@@ -41,8 +46,9 @@ def handman():
             print(f"You already guesed letter: ''{user_guess_letter}''")
             lives -= 1
         else: 
-            print("got got wrong")
+            print(f"''{user_guess_letter}'' not in the word")
             lives -= 1
+        users_letters.append(user_guess_letter)
 
     if len(chosen_word_letters) == 0 and lives > 0:
         print(f"Congratulation! You guess the word ''{chosen_word}'' correctly")
